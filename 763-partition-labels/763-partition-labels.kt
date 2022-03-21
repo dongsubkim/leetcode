10fa49b17e4a2f914d4f543478b4c0bcd5ff9b0a
Dong Sub Kim <ds.kim@berkeley.edu>
2022-03-22 03:44:16 +0900

    Time: 296 ms (44.34%), Space: 36.5 MB (36.79%) - LeetHub

--- 763-partition-labels/763-partition-labels.kt
+++ 763-partition-labels/763-partition-labels.kt
@@ -3,23 +3,21 @@ import kotlin.math.max
 
 class Solution {
     fun partitionLabels(s: String): List<Int> {
-        val lastIndex = mutableListOf<Int>()
-        for (i in 0..26) {
-            lastIndex.add(-1)
-        }
-        s.forEachIndexed { index, c ->
-            lastIndex[c - 'a'] = index
-        }
-        val answer = mutableListOf<Int>()
+        val lastIndex = IntArray(26)
         var index = 0
         var anchor = 0
-        for (i in s.indices) {
-            index = max(index, lastIndex[s[i] - 'a'])
-            if (i == index) {
-                answer.add(i - anchor + 1)
-                anchor = i + 1
-            }
+        s.forEachIndexed { i, c ->
+            lastIndex[c - 'a'] = i
         }
-        return answer.toList()
+        return s.mapIndexed { i, c ->
+            index = max(index, lastIndex[c - 'a'])
+            if (i == index) {
+                val size = i - anchor + 1
+                anchor = i + 1
+                size
+            } else {
+                null
+            }
+        }.filterNotNull()
     }
 }
