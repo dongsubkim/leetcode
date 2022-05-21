5933f4f4c3bf2043101892c19e4c41b0a6db403a
Dong Sub Kim <ds.kim@berkeley.edu>
2022-05-21 15:38:06 +0900

    Time: 480 ms (32.74%), Space: 100.2 MB (17.25%) - LeetHub

--- 322-coin-change/322-coin-change.kt
+++ 322-coin-change/322-coin-change.kt
@@ -1,28 +1,16 @@
+import kotlin.math.min
+
 class Solution {
     
-    val cache = mutableMapOf(0 to 0)
-    
     fun coinChange(coins: IntArray, amount: Int): Int {        
-        coins.forEach { coin -> 
-            cache[coin] = 1
+        val dp = IntArray(amount + 1) { i -> 100000 }
+        dp[0] = 0
+        
+        coins.filter { it <=  amount }.forEach { dp[it] = 1 }
+        
+        (1..amount).forEach { i -> 
+            coins.filter { it <= i }.forEach { dp[i] = min(dp[i], dp[i-it] + 1) }
         }
-        return solve(coins.toList(), amount)
-    }
-    
-    fun solve(coins: List<Int>, amount: Int): Int {
-        if (cache.containsKey(amount)) return cache[amount]!!
-        
-        val minCount = coins.map { coin ->
-            if (coin > amount) {
-                -1
-            } else {
-                val sub = solve(coins, amount - coin)
-                if (sub == -1) -1
-                else sub + 1
-            }
-        }.filter { it != -1 }.min() ?: -1
-        
-        cache[amount] = minCount
-        return minCount
+        return if (dp[amount] == 100000) -1 else dp[amount]
     }
 }
