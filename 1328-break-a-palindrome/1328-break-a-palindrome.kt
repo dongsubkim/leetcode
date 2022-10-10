8e176110b8fa3e3c1e21ab7db8fa35c365a99f94
Dong Sub Kim <ds.kim@berkeley.edu>
2022-10-10 19:11:01 +0900

    Time: 294 ms (25.00%), Space: 36 MB (25.00%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun breakPalindrome(palindrome: String): String {
+        if (palindrome.length == 1) return ""
+        
+        for ((index, char) in palindrome.withIndex()) {
+            if (char != 'a' && index != (palindrome.length / 2)) {
+                return palindrome.take(index) + "a" + palindrome.drop(index + 1)
+            }            
+        }
+        
+        return palindrome.slice(0 .. (palindrome.lastIndex - 1)) + "b"
+    }
+}
